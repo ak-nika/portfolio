d5d98ed65cac38b9f3beedb3f32751b92d14a559
ak-nika <akingbayiojo@gmail.com>
2024-05-14 15:18:48 +0100

    draft done

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Web Designer", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+  loopCount: Infinity,
+  showCursor: true,
+  cursorChar: "|",
+  smartBackspace: true,
+});
